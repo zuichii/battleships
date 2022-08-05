e0380631a1fd3ebae3288ed507afbb3fc80797f7
zuichii <ntc.cordero@gmail.com>
2022-08-05 14:15:36 +0930

    q1 fixed

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,12 +6,14 @@ int sum_diagonal(int array[4][4]){
 
 
     for(int i = 0; i < 4; i++){
-        for(int j = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
             if(i == j){
                 sum = sum + array[i][j];
+                
             }
             
         }
+      
     }
 
     return sum;
