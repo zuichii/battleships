750432df8181f004573d38df8bc99cf46c80ca7b
zuichii <ntc.cordero@gmail.com>
2022-08-05 18:30:25 +0930

    proper printing

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -5,13 +5,19 @@ using namespace std;
 
 void print_summed(int array1[3][3],int array2[3][3]){
 
-    int sum = 0;
+    
 
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            sum = sum + array1[i][j] + array2[i][j];
+            if(j == 0 || j == 1){
+                cout << array1[i][j] + array2[i][j] << " ";
+            }
+            else{
+                cout << array1[i][j] + array2[i][j];
+            }
+            
         }
+        cout << endl;
     }
-    cout << sum;
 
 }
