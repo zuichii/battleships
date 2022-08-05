bcb8a93da2dc16af6d646c507b363b165b372034
zuichii <ntc.cordero@gmail.com>
2022-08-05 14:04:49 +0930

    q1

---
+++ function-1-1.cpp
@@ -0,0 +1,19 @@
+// function that return the sum of the elements along the main diagonal
+
+int sum_diagonal(int array[4][4]){
+
+    int sum;
+
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; i < 4; i++){
+            if(i == j){
+                sum = sum + array[i][j];
+            }
+            
+        }
+    }
+
+    return sum;
+
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdlib.h>
+
+extern int sum_diagonal(int array[4][4]);
+
+int main(){
+    int array[4][4] = {{1,2,3,4,},{1,2,3,4},{1,3,4,5},{1,2,3,4}};
+
+    
+
+    std::cout << "The number is: " << sum_diagonal(array) << std::endl;
+
+    return 0;
+}
+
+
+
