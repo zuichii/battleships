63f5f32d66bd2e21c94a8e75c1b0d43cfe53fc76
zuichii <ntc.cordero@gmail.com>
2022-08-05 15:12:43 +0930

    it works

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -2,33 +2,29 @@
 
 int is_identity(int array[10][10]){
 
-    bool valid = true;
+    int valid = 1;
 
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
-            if(i == j && array[i][j] == 1){
-                valid = true;
-            }
-            else{
+            
+            if(i != j && array[i][j] != 0){
                 return 0;
             }
-
-
-            if(i != j && array[i][j] == 0){
-                valid = true;
+            
+            
+            if(i == j && array[i][j] != 1){
+                return 0;
             }
-            else{
-                return false;
-            }
+        
         
         }
     }
 
-    if(valid == true){
+    if(valid  == true){
         return 1;
     }
-
-    return 0;
-
+    else{
+        return 0;
+    }
 
 }
