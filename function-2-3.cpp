a89e219897ab52dcf75c9243bc4b77f7162a6aa7
zuichii <ntc.cordero@gmail.com>
2022-08-05 21:13:17 +0930

    q3

---
+++ function-2-3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+
+bool is_palindrome(int integers[], int length){
+    bool valid = true;
+
+    for(int i = 0; i < length; i++){
+        if(integers[i] != integers[length-i-1]){
+            valid = false;
+            break;
+        }
+    }
+    return valid;
+}
+
+
+int sum_array_elements(int integers[], int length){
+    int sum;
+    if(length <= 0){
+        return -1;
+    }
+    else{
+        for(int i = 0; i < length; i++){
+        sum+= integers[i];
+        }
+    }
+    return sum;
+
+}
+
+
+int sum_if_palindrome(int integers[], int length){
+    bool p = is_palindrome(integers, length);
+    if(p = true){
+        return sum_array_elements(integers, length);
+    }
+    else{
+        return -2;
+    }
+    return 0;
+}
---
+++ main-2-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+extern int sum_if_palindrome(int integers[], int length);
+
+int main(){
+    int integers[5] = {1,2,3,4,5};
+    int length = 5;
+
+    sum_if_palindrome(integers, length);
+    
+
+  
+}
