07d22cf624a511a102adbc748b0621bd5c856cde
zuichii <ntc.cordero@gmail.com>
2022-08-05 18:04:50 +0930

    q4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -5,7 +5,13 @@ using namespace std;
 void print_scaled(int array[3][3],int scale){
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            cout << array[i][j] * scale << " ";
+            if(j == 0 || j == 1){
+                cout << array[i][j] * scale << " ";
+            }
+            else{
+                cout << array[i][j] * scale;
+            }
+            
         }
         cout << endl;
     }
