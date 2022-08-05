c0aba330e319c9c355edb8da8d67e7dfd7e2cb6c
zuichii <ntc.cordero@gmail.com>
2022-08-05 21:36:49 +0930

    q3

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 
-extern int is_palindrome(int integers[], int length);
+extern int sum_if_palindrome(int integers[], int length);
 
 int main(){
     int integers[5] = {1,2,3,2,1};
     int length = 5;
 
-    cout << is_palindrome(integers, length);
+    cout << sum_if_palindrome(integers, length);
     
 
   
