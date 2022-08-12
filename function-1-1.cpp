57bd5b48e974cf3acb8030171d567b3ac77e97fb
zuichii <ntc.cordero@gmail.com>
2022-08-12 14:41:22 +0930

    q1

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -5,7 +5,12 @@ using namespace std;
 void print_matrix(int array[10][10]){
     for (int i = 0; i < 10; i++){
         for (int j = 0; j < 10; j++){
-            cout << " " << array[i][j];
+            if (i == 0){
+                cout << array[i][j];
+            }
+            else if (i > 0){
+                cout << array[i][j] << " ";
+            }
         }
         cout << endl;
     }
