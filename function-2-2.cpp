6e1b4947b6448f2301075d584ba8947f6d4a8862
zuichii <ntc.cordero@gmail.com>
2022-08-12 14:51:55 +0930

    q2-2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,14 +1,13 @@
-#include <math.h>
-int bin_to_int(int binary_digits[], int number_of_digits){
-    
-    int n = number_of_digits;
-    int sum = 0;
-    
-    for(int i = 0; i < n; i++){
-        sum = sum + binary_digits[i] * pow(2,n-1);
-        n--;
-    }
-    return sum;
 
+#include <cmath>
+
+int binary_to_int(int binary_digits[], int number_of_digits) {
+  int sum = 0, ans = 0;
+
+  for (int i = 0; i < number_of_digits; i++) {
+    ans = (binary_digits[i]) * pow(2, number_of_digits - 1);
+    number_of_digits--;
+    sum = sum + ans;
+  }
+  return sum;
 }
-
