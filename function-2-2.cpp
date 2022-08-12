e8c12f4f3d602adf13892c6bdc224c0a7b904c33
zuichii <ntc.cordero@gmail.com>
2022-08-12 15:20:07 +0930

    q2

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -9,7 +9,7 @@ int bin_to_int(int binary_digits[], int number_of_digits) {
   int n = number_of_digits;
   int power = 0;
 
-  for (int i = n; i > 0; i--) {
+  for (int i = number_of_digits - 1; i > 0; i--) {
     ans = (binary_digits[i]) * pow(2, power);
     power ++;
     sum = sum + ans;
