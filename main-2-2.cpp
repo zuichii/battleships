d9ec23037660fe2be09a76d1e72b60ee29f8d5f8
zuichii <ntc.cordero@gmail.com>
2022-08-15 11:05:40 +0930

    cleaning workshop4

--- main-2-2.cpp
+++
@@ -1,9 +0,0 @@
-extern int bin_to_int(int binary_digits[], int number_of_digits);
-
-int main(){
-    int binary_digits[] = {0,1,0};
-    int number_of_digits = 3;
-
-    bin_to_int(binary_digits, number_of_digits);
-
-}
