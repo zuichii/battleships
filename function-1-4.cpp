a38aecf2cfaf10d4cfa82a73229ba4b3ffb0d6e7
zuichii <ntc.cordero@gmail.com>
2022-08-15 11:20:42 +0930

    workshop4

---
+++ function-1-4.cpp
@@ -0,0 +1,4 @@
+int size_of_variable_star_arr(){
+    int *arr = new int[3];
+    return sizeof(arr);
+}
---
+++ main-1-4.cpp
@@ -0,0 +1,5 @@
+extern int size_of_variable_star_arr();
+int main(){
+    size_of_variable_star_arr();
+    return 0;
+}
