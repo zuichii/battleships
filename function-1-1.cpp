757ed2ada141bace25b243999058bc2e22f0d4f7
zuichii <ntc.cordero@gmail.com>
2022-08-15 11:16:06 +0930

    workshop4

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,4 +1,4 @@
 int size_of_variable_star_t(){
     int *t = new int;
-    return sizeof(*t);
+    return *t * sizeof(int);
 }
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,5 +1,6 @@
 extern int size_of_variable_star_t();
 int main(){
     size_of_variable_star_t();
+    
     return 0;
 }
