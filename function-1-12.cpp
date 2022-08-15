bca534b02effa39199b2e2e5d6e74d2eb059c746
zuichii <ntc.cordero@gmail.com>
2022-08-15 12:07:43 +0930

    q5

---
+++ function-1-12.cpp
@@ -0,0 +1,5 @@
+int size_of_variable_star_arr(){
+    int *arr = new int[3];
+    delete arr;
+    return sizeof(arr);
+}   
---
+++ main-1-12.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+extern int size_of_variable_star_arr();
+int main(){
+    std:: cout << size_of_variable_star_arr();
+    
+    return 0;
+}
