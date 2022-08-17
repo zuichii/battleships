2b421386a8772715934ff3af38891b652a32d5c9
zuichii <ntc.cordero@gmail.com>
2022-08-17 19:25:23 +0930

    q1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <string>
-using namespace std;
+
 extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
 
 int main(){
+    int n = 3;
+    std::string first[][2] = {{"Green","Blue"}, {"Yellow", "Red"}, {"Purple", "Pink"}};
+    std::string second[n][2];
     
+    return 0;
+
 }
