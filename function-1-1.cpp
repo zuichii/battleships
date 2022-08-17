76de06fc7520108a48f97e9a9c9dbfad1547a455
zuichii <ntc.cordero@gmail.com>
2022-08-17 19:08:56 +0930

    q1

---
+++ function-1-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void string_2d_copy(std::string first[][2], std::string second[][2], int n){
+    for(int i = 0; i < 3; i++ ){
+        for(int j = 0; i < 2; i++){
+            second[i][j] = first[i][j];
+        }
+    }
+    return;
+}
---
+++ main-1-1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+extern void string_2d_copy(std::string first[][2], std::string second[][2], int n);
+
+int main(){
+    
+}
