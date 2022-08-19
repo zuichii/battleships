890e13b1391dbc6fd8a019159747ae7410469fd0
zuichii <ntc.cordero@gmail.com>
2022-08-19 16:02:50 +0930

    q4

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,14 +1,28 @@
-
+#include <iostream>
+using namespace std;
 void copy_doubles(double *old_array,double *new_array,int length){
     
   
 
+    double *n;
     
     
+    //for(int i = 0; i < length; i++){
+        //n = old_array + i;
+        //*(new_array + i) = *n;
+        
+        
+    //}
+
     for(int i = 0; i < length; i++){
-        *(old_array + i) = *(new_array + i);
+        *(old_array+i) = *(new_array+i);
+        
         
     }
+
+
+
+    cout << n << endl << *n;
     return;
 
 }
