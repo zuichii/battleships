ad4217e981caa72e888e9a538a898543de994726
zuichii <ntc.cordero@gmail.com>
2022-08-19 16:19:52 +0930

    q2-1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -4,8 +4,7 @@ void multiples_of_seven(int *nums,int length){
     for(int i = 0; i < length; i++){
         if(*(nums + i) % 7 == 0){
             cout << *(nums + i);
-            cout << endl;
-
+            cout << "\n";
         }
     }
     return;
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 extern void multiples_of_seven(int *nums,int length);
 int main(){
     int length = 3;
-    int array[3] = {7, 50, 1000};
-    int *nums = array;
+    int vector[3] = {7, 50, 1000};
+    int *nums = vector;
 
     multiples_of_seven(nums, length);
     
