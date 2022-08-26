e01969a4dafbc7d2d4aefda5b7eee4b1efd7c6dc
zuichii <ntc.cordero@gmail.com>
2022-08-26 14:32:28 +0930

    p2

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -2,16 +2,16 @@ int tax_bracket_lookup(int income){
     if(income <= 10000){
         return 0;
     }
-    if(income > 10000 || income <= 20000){
+    else if(income > 10000 || income <= 20000){
         return 10;
     }
-    if(income > 20000 || income <= 50000){
+    else if(income > 20000 || income <= 50000){
         return 15;
     }
-    if(income > 50000 || income <= 1000000){
+    else if(income > 50000 || income <= 1000000){
         return 20;
     }
-    if(income > 1000000){
+    else if(income > 1000000){
         return 30;
     }
 }
