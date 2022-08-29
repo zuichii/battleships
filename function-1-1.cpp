dfc3ea9249d017f684de1e18d2eede7a294e796a
zuichii <ntc.cordero@gmail.com>
2022-08-29 11:14:47 +0930

    q3

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
-
 int **pointer_array(int *vals, int len){
-    int **val_ptrs;
-    vals = new int[len];
+    int** val_ptrs;
+    
+    
+    *val_ptrs = new int[len];
+    
     for(int i = 0; i < len; i++){
         *val_ptrs = vals+i;
     }
+
+
+
     return val_ptrs;
 
 }
