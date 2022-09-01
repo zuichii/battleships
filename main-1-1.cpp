df2bb71c5e16aa468df0ef3a53cc7900a4213fee
zuichii <ntc.cordero@gmail.com>
2022-09-01 16:49:31 +0930

    prac4

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,5 +3,19 @@
 using namespace std;
 
 int main(){
-    Capybara samir;
+    Capybara a, b, c, d;
+
+    a.Capybara::setName("samir");
+    b.Capybara::setName("nate");
+    c.Capybara::setName("kosta");
+    d.Capybara::setName("dylan");
+
+    a.Capybara::setAge(20);
+    b.Capybara::setAge(19);
+    c.Capybara::setAge(19);
+    d.Capybara::setAge(20);
+
+    
+
+
 }
